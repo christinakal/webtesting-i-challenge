125c2d9ebd221a7135cc5e6b57d4cbaa1dd5a622
christina kaliora <develop@pirron.one>
2020-03-26 14:18:44 +0100

    finished tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,12 +1,36 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 
 // test away!
-describe('fail', () => {
-    it('runs without crashing', () => {
-        enhancer.repair({ name:'item',
-        durability: 2,
-        enhancement: 50})
-    })
+describe("enhancer", () => {
+  describe("repair", () => {
+    const testItem = {
+      durability: 50,
+      enhancement: 1,
+      name: "Test Item"
+    };
 
+    it("should throw if item is not the proper format", () => {
+      [{}, true, 1, "string", []].forEach(item => {
+        expect(() => repair(item)).toThrow();
+      });
+    });
 
-})
+    it("should restore durability to 100", () => {
+      expect(enhancer.repair(testItem).durability).toEqual(100);
+    });
+  });
+
+  describe("succeed", () => {
+    const enhanceableItem = {
+      durability: 100,
+      enhancement: 5,
+      name: "Enhanceable Item"
+    };
+
+    it("should throw on improper input", () => {
+      [{}, true, 1, "string", []].forEach(item => {
+        expect(() => succeed(item)).toThrow();
+      });
+    });
+  });
+});
