88380e688f4af808401c6c04749a68d457c4eb12
christina kaliora <develop@pirron.one>
2020-03-26 14:31:57 +0100

    small fixes

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -9,17 +9,12 @@ describe("enhancer", () => {
       name: "Test Item"
     };
 
-    it("should throw if item is not the proper format", () => {
-      [{}, true, 1, "string", []].forEach(item => {
-        expect(() => repair(item)).toThrow();
-      });
-    });
-
     it("should restore durability to 100", () => {
       expect(enhancer.repair(testItem).durability).toEqual(100);
     });
   });
 
+  // === succeed
   describe("succeed", () => {
     const enhanceableItem = {
       durability: 100,
@@ -29,8 +24,62 @@ describe("enhancer", () => {
 
     it("should throw on improper input", () => {
       [{}, true, 1, "string", []].forEach(item => {
-        expect(() => succeed(item)).toThrow();
+        expect(() => enhancer.succeed(item)).toThrow();
       });
+    });
+
+    it("should add 1 to enhancmenet for enhanceable items", () => {
+      expect(enhancer.succeed(enhanceableItem).enhancement).toEqual(6);
+    });
+  });
+
+  // === fail
+  describe("fail", () => {
+    const baseItem = {
+      durability: 100,
+      enhancement: 5,
+      name: "Base Item"
+    };
+
+    it("should decrease durability by 5 if enhancement is less than 15", () => {
+      expect(enhancer.fail(baseItem).durability).toEqual(95);
+    });
+
+    it("should decrease durability by 10 if enhancement is >= 15", () => {
+      expect(
+        enhancer.fail({ ...baseItem, enhancement: 15 }).durability
+      ).toEqual(90);
+      expect(
+        enhancer.fail({ ...baseItem, enhancement: 20 }).durability
+      ).toEqual(90);
+    });
+
+    it("should decrease enhancement by 1 if enhancement >=17", () => {
+      expect(
+        enhancer.fail({ ...baseItem, enhancement: 17 }).enhancement
+      ).toEqual(16);
+      expect(
+        enhancer.fail({ ...baseItem, enhancement: 18 }).enhancement
+      ).toEqual(17);
+    });
+  });
+
+  // === get
+  describe("get", () => {
+    const baseItem = {
+      name: "Base Item",
+      enhancement: 10,
+      durability: 90
+    };
+
+    it("should not modify name if enhancement is 0", () => {
+      expect(enhancer.get({ ...baseItem, enhancement: 0 }).name).toEqual(
+        baseItem.name
+      );
+    });
+
+    it("should prepend enhancement level to name if enhancement > 0", () => {
+      expect(enhancer.get(baseItem).name).toEqual("[+10] " + baseItem.name);
     });
   });
 });
