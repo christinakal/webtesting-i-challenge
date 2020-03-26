97d90c0ed4738e5ac6a9fa974e686e0d3e924b06
christina kaliora <develop@pirron.one>
2020-03-26 14:08:58 +0100

    finished enhancer

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,33 +2,62 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
-const object = {
-  name:'Christina',
-  durability: 2,
-  enhancement: 50
-}
-
-
- 
 function succeed(item) {
-  return { ...item };
+  const { enhancement } = item;
+
+  if (enhancement >= 0 && enhancement <= 20) {
+    if (enhancement < 20) {
+      return { ...item, enhancement: enhancement + 1 };
+    } else {
+      return { ...item };
+    }
+  } else {
+    throw 'Enhancement value out of range. Must be between 0 and 20 inclusive';
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  const { durability, enhancement } = item;
+  const newItem = { ...item };
+
+  if (durability === undefined || enhancement === undefined) {
+    throw 'Fail expects durability and enhancement properties';
+  }
+
+  if (enhancement < 15) {
+    newItem.durability -= 5;
+  } else if (enhancement >= 15) {
+    newItem.durability -= 10;
+    if (enhancement >= 17) {
+      newItem.enhancement -= 1;
+    }
+  }
+
+  return newItem;
 }
 
 function repair(item) {
-  // accepts an item object and returns a new item with the durability restored to 100
-  return item.durability = 100;
+  if (item.durability !== undefined) {
+    return { ...item, durability: 100 };
+  } else {
+    throw 'Unexpected Item Format';
+  }
 }
 
 function get(item) {
-  return { ...item };
+  const { name, enhancement } = item;
+  if (name === undefined || enhancement === undefined) {
+    throw 'Get expects item.name and item.enhancement to be defined';
+  }
+
+  if (enhancement === 0) {
+    return { ...item };
+  }
+
+  if (enhancement > 0) {
+    return { ...item, name: `[+${enhancement}] ${name}` };
+  }
 }
-
-
-console.log(repair(object))
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -8,8 +8,5 @@ describe('fail', () => {
         enhancement: 50})
     })
 
-    // it('accept an object as argument', () => {
-    //     const expected = { name:'item', durability: 2, enhancement: 50};
-    //     expect(enhancer.repair({ })).toHaveBeenCalledWith(expected);
-    // })
+
 })
